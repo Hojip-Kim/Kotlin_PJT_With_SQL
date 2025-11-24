559686e9f6ed75912317c8e80de24be26c170b70
Hojip-Kim <rlaghwlq12@naver.com>
2025-11-24 10:57:56 +0900

    feat : kotlin settings init

---
+++ src/main/kotlin/org/kotlin/kotlin_pjt/KotlinPjtApplication.kt
@@ -0,0 +1,11 @@
+package org.kotlin.kotlin_pjt
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KotlinPjtApplication
+
+fun main(args: Array<String>) {
+    runApplication<KotlinPjtApplication>(*args)
+}
