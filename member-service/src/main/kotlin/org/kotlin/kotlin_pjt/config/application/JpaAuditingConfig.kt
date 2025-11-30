5bd988e95662419980c45d7c9c3ec9080e9ac48a
Hojip-Kim <rlaghwlq12@naver.com>
2025-11-30 14:01:52 +0900

    #3 feat : JPA Config feature

---
+++ member-service/src/main/kotlin/org/kotlin/kotlin_pjt/config/application/JpaAuditingConfig.kt
@@ -0,0 +1,8 @@
+package org.kotlin.kotlin_pjt.config.application
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing
+
+@Configuration
+@EnableJpaAuditing
+class JpaAuditingConfig
